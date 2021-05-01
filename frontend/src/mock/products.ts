9390a709c41437cc2fa36b194f1607aea5696a0d
magdalena-u <magdalena.urzon@gmail.com>
2021-05-01 16:57:34 +0200

    SKL-001 move images to S3 bucket and remove them from public, remove mock

--- frontend/src/mock/products.ts
+++
@@ -1,8 +0,0 @@
-export const products = [
-  { title: "Bottle", price: 69.0, imageUrl: "/image/bottle1.jpg" },
-  { title: "Mug", price: 25.0, imageUrl: "/image/mug.png" },
-  { title: "Scissors", price: 15.0, imageUrl: "/image/scissors.png" },
-  { title: "Watch", price: 150.0, imageUrl: "/image/watch.png" },
-  { title: "Dish set", price: 300.0, imageUrl: "/image/set.jpg" },
-  { title: "Pot", price: 89.0, imageUrl: "/image/pot.jpeg" },
-];
